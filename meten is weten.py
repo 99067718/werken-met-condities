6e587615f70a2dcd63d1d0092598465cd83da530
unknown <99067718@mydavinci.nl>
2021-09-14 09:40:26 +0200

    Min en Max

---
+++ meten is weten.py
@@ -0,0 +1,7 @@
+a = int(input("Insert a number here --> "))
+b = int(input("Insert number 2 here --> "))
+if a > b:
+    print(str(a) + " is gevolgd door de waarde van "+ str(b))
+
+else:
+    print(str(b)+ " is gevolgd door de waarde van " + str(a))
